5f68631e312416ceacbdd9b101eace425c25bf54
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-03-21 09:45:53 -0700

    make some changes on views

---
+++ routes/books.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+// All Books Route
+router.get('/', async (req, res) => {
+    
+  })
+
+// New book route, we will render new author page
+router.get('/new', (req, res) => {
+    
+  })
+
+// Create book Route
+router.post('/', async (req, res) => {
+    
+  })
+
+
+
+module.exports = router
