1f216b6da34cf296fb8da01be66acb8fd3bc8819
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-03-22 20:20:41 -0700

    finish the back end

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,13 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  )
+  
+  FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+  })
+  //turn all file input elements into ponds
+  FilePond.parse(document.body);
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
+  require('dotenv').load()
+}
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
-
-
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
@@ -31,6 +29,5 @@ db.once('open', () => console.log('Connected to Mongoose'))
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
-
 
 app.listen(process.env.PORT || 3000)
